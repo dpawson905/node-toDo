78da43e1b94624f3bb883d278aee0d0467b85ef2
Darrell Pawson <dpawson905@gmail.com>
2018-01-07 00:27:35 -0500

    Refactor files and set up server.js

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.listen(3000, () => {
+  console.log('Started on port 3000');
+});
