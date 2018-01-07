92f81dfb791829330fb8f7aa34cc98766e04b1b9
Darrell Pawson <dpawson905@gmail.com>
2018-01-06 23:01:09 -0500

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,26 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log("Unable to connect to the database!");
+  }
+  console.log("Connected to MongoDB Server");
+
+//  db.collection('Todos').find({
+//    _id: new ObjectID('5a51924d4ee08c3408ac625a')
+//  }).toArray().then((docs) => {
+//   console.log('Todos');
+//   console.log(JSON.stringify(docs, undefined, 2));
+//  }, (err) => {
+//   console.log('Unable to fetch todos', err);
+//  });
+
+ db.collection('Todos').find().count().then((count) => {
+  console.log(`Todos Count: ${count}`);
+ }, (err) => {
+  console.log('Unable to fetch todos', err);
+});
+
+  // db.close();
+});
